bf41bc242a959ee26a711a8c22f50c281f3724d4
Marta <marta.nunes.abreu@gmail.com>
2021-01-17 12:17:20 +0000

    created File class

---
+++ file_io.cpp
@@ -0,0 +1,63 @@
+/* Source code for reading in files */
+
+#include <cstdio>
+#include <iostream>
+#include <istream> // for getline
+#include <sstream> // for istringstream
+#include "file_io.h"
+
+void File::set_filename(std::string name)
+{
+    this->filename = name;
+}
+
+std::string File::get_filename()
+{
+    return this->filename;
+}
+
+void File::set_details(std::string name, char delimitor, bool headers)
+{
+    this->filename = name;
+    this->delimitor = delimitor;
+    this->headers = headers;
+}
+
+bool File::open_file()
+{
+    std::ifstream file_handle(this->filename);
+    if ( !file_handle.is_open() )
+    {
+       return NOK;
+    }
+
+    std::string row;
+    std::string entry;
+    bool first_line = true;
+
+    while (std::getline(file_handle, row))
+    {
+        this->rows.push_back(row);
+        std::istringstream full_line(row);
+        while (std::getline(full_line, entry, this->delimitor))
+        {
+            if (this->headers && first_line)
+            {
+                this->header_row.push_back(entry);
+                first_line = false;
+            }
+            else
+            {
+                this->single_entries.push_back(entry);
+            }
+            std::cout << entry << ' ';
+        }
+        std::cout << std::endl;
+    }
+    return OK;
+}
+
+bool File::process_rows()
+{
+
+}
--- main.cpp
+++ main.cpp
@@ -5,84 +5,44 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include "file_io.h"
 
-const bool OK = true;
-const bool NOK = false;
-
-std::string get_user_filename()
+void get_file_details(File *file_ptr)
 {
     std::string filename;
+    char delimitor;
+    char headers;
 
-    std::cout << "Enter the name of the file you wish to process: " << std::endl;
+    std::cout << "Welcome to the file processing program" << std::endl;
+    std::cout << "Name of file to open: " << std::endl;
     std::cin >> filename;
 
-    std::cout << "Opening file: " << filename << std::endl;
-    // check if file exists in directory
-    return filename;
-}
-
-
-char get_file_delimitor()
-{
-    char delimitor;
-    std::cout << "Enter the type of delimitor: " << std::endl;
+    std::cout << "Delimitor used in file [,] or [-] or [|] " << std::endl;
     std::cin >> delimitor;
-    return delimitor;
-}
 
-void ask_for_headers(void)
-{
-    char has_headers;
-    std::cout << "Does the file have headers? (y/n)" << std::endl;
-    std::cin >> has_headers;
-    if ( has_headers == 'y' )
-    {
-        // mark headers flag;
-    }
-    else {
-        std::cout << "proceeding with normal file processing..." << std::endl; 
-        return;
-    }  
+    std::cout << "Does it have headers in first row (y/n) " << std::endl;
+    std::cin >> headers;
+
+    file_ptr->set_details(filename, delimitor, headers);
+
+    return;
 }
 
 int main()
 {
-    std::cout << "Welcome to the file processing program" << std::endl;
-    std::string filename = get_user_filename();
-    char delim = get_file_delimitor();
+    // std::string filename = get_user_filename();
+    File my_file;
+    get_file_details(&my_file);
 
-    ask_for_headers();
-    // ask if file has headers
-    // std::string headers;
-    // std::cout << "If the file has headers, please enter these in sequence separated by space" << std::endl;
-    // std::cout << "Otherwise enter 'N' " << std::endl;
-    // std::getline(std::cin, headers);
-
-    std::ifstream file(filename);
-
-    //std::vector<std::string> file_lines;
-    //std::vector<std::string> words;
-    if (file.is_open())
+    if ( my_file.open_file() == OK )
     {
-        std::string line;
-        while ( std::getline(file,line) )
-        {
-            std::istringstream full_line(line);
-            std::string word;
-            while( std::getline(full_line, word, delim))
-            {
-                std::cout << word << ' ';
-            }
-            std::cout << std::endl;
-        }
-        file.close();
+
     }
-    else std::cout << "Unable to open file"; 
+
 
 
     /* 
     for ( std::string x: words) {
         std::cout << x << " " << std::endl;
     } */
-
 }
